bd0a8ebe8515a473aaf8acffaf5f9134b4fe26b6
PaulHaze <dev@artemist.com.au>
2022-04-30 04:55:43 +1000

    first ui cypress test working

--- cypress/integration/ui/bookish.spec.js
+++ cypress/integration/ui/bookish.spec.js
@@ -1,5 +1,5 @@
 describe('Bookish Application', () => {
-  it('Visits the Bookish', () => {
+  it('Visits Bookish and finds an h2 with "Bookish"', () => {
     cy.visit('http://localhost:3000/');
     cy.get('h2[data-test="heading"]').contains('Bookish');
   });
