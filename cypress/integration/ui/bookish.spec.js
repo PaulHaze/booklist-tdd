f7a2455aefe28e22536120e29442b4110ee2642e
PaulHaze <dev@artemist.com.au>
2022-04-30 04:41:28 +1000

    add first test for heading in cypress

---
+++ cypress/integration/ui/bookish.spec.js
@@ -0,0 +1,6 @@
+describe('Bookish Application', () => {
+  it('Visits the Bookish', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('h2[data-test="heading"]').contains('Bookish');
+  });
+});
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,9 @@
 export function Home() {
   return (
-    <div className="h-[100vh] bg-gradient-to-br from-teal-700/50 to-sky-500/50 flex items-center justify-center p-md">
-      <div className="flex flex-col bg-white rounded-lg shadow-2xl">
-        <h2 className="m-10 text-[#504e63] font-bold text-center">
-          Im your new Tailwind Project
-        </h2>
-        <p className="text-[#504e63] text-center mb-10">
-          Delete this and make something great
-        </p>
-      </div>
+    <div className="h-[100vh] bg-gradient-to-br from-teal-700/20 to-sky-500/30 flex  p-md">
+      <section>
+        <h2 data-test="heading">Bookish</h2>
+      </section>
     </div>
   );
 }
