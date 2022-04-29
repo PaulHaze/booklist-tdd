ac7ec6f8a8ce4485a866ad5c0ca011bfbd750b2f
PaulHaze <dev@artemist.com.au>
2022-04-30 05:38:20 +1000

    add a basic test for chekcing the booklist

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,10 @@ export function Home() {
     <div className="h-[100vh] bg-gradient-to-br from-teal-700/20 to-sky-500/30 flex  p-md">
       <section>
         <h2 data-test="heading">Bookish</h2>
+        <div data-test="book-list">
+          <div className="book-item">Item one</div>
+          <div className="book-item">Item Two</div>
+        </div>
       </section>
     </div>
   );
