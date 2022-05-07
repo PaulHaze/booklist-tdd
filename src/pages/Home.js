b98c34dd71a67dadfed715f95bde8c1c3e1cc630
PaulHaze <dev@artemist.com.au>
2022-05-08 08:09:17 +1000

    remove old methods from home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from '@mantine/core';
-
 import axios from 'axios';
 
-import { BookList } from 'components';
 import { BookListContainer } from 'features';
-
-import { books } from 'data/books';
 
 export function Home() {
   const [localBooks, setLocalBooks] = useState([]);
@@ -21,28 +16,6 @@ export function Home() {
       fetchBooks();
     }
   }, []);
-
-  const handleReload = () => {
-    books.forEach(book =>
-      axios.post('http://localhost:8080/books', book, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }),
-    );
-  };
-
-  const handleDelete = () => {
-    axios.delete('http://localhost:8080/books?_cleanup=true').catch(err => err);
-  };
-
-  const handleClear = () => {
-    setLocalBooks([]);
-  };
-
-  const handleFetch = () => {
-    fetchBooks();
-  };
 
   return (
     <div className="h-[100vh] bg-gradient-to-br from-teal-700/20 to-sky-500/30 flex flex-col p-md">
