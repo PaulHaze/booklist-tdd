aae36785b63777bc2135a45ef05e45cf2f61a0ce
PaulHaze <dev@artemist.com.au>
2022-05-09 10:49:10 +1000

    pre remote service hook refactor

--- src/features/BookDetailsContainer.js
+++ src/features/BookDetailsContainer.js
@@ -1,9 +1,11 @@
+// import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { BookDetails } from './BookList/BookDetails';
 
 export function BookDetailsContainer() {
   const { id } = useParams();
+
   // const book = data.find(b => b.id === id);
   // console.log(book);
   return (
--- src/features/BookList/BookDetails.js
+++ src/features/BookList/BookDetails.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 export function BookDetails({ id }) {
+  const [book, setBook] = useState({});
+
+  const fetchBook = async () => {
+    try {
+      const res = await axios.get(`http://localhost:8080/books/${id}`);
+      setBook(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchBook();
+  }, [id]);
+
   return (
-    <div className="book-item bg-white rounded border my-3 p-3">
-      Details for Book Id {id}
+    <div className="book-item bg-white rounded border my-3 p-6">
+      <h2 className="book-title">{book.name || 'Loading'}</h2>
     </div>
   );
 }
