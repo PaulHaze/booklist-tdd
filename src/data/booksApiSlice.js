18e2ee800cc2fef50f7c8ae6f215cb344903e232
PaulHaze <dev@artemist.com.au>
2022-05-11 18:33:06 +1000

    export API hooks

--- src/data/booksApiSlice.js
+++ src/data/booksApiSlice.js
@@ -1,8 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-// http://localhost:8080/books
-// ?q=${searchTerm}
-
 export const booksApi = createApi({
   reducerPath: 'booksApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/books' }),
@@ -15,3 +12,5 @@ export const booksApi = createApi({
     }),
   }),
 });
+
+export const { useGetBookDetails, useGetBooksBySearchTerm } = booksApi;
